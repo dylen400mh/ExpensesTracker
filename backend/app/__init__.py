cae403f3c05bf3431d2e668a01adaf7ee2bb54f3
dylen400mh <dylenbelanger2004@gmail.com>
2023-11-22 16:00:04 -0500

    Create login manager

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 # create flask application instance
 app = Flask(__name__)
@@ -9,6 +10,9 @@ app.config.from_pyfile("config.py")
 
 # initialize SQLAlchemy
 db = SQLAlchemy(app)
+
+# create login manager
+login_manager = LoginManager(app)
 
 # import routes
 from app import routes
