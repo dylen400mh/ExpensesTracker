02e5446047f835493d0f0b1de7fcac6d27322d87
dylen400mh <dylenbelanger2004@gmail.com>
2023-11-21 19:57:28 -0500

    Create config.py

---
+++ backend/app/config.py
@@ -0,0 +1,14 @@
+import os
+import secrets
+
+# Flask configuration
+DEBUG = True  # Set to False in production
+SECRET_KEY = secrets.token_hex(16)  # Generate a secure random value
+
+# SQLAlchemy configuration
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///example.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# CORS configuration (if needed)
+# CORS_ALLOW_HEADERS = 'Content-Type'
+# CORS_RESOURCES = {r'/api/*': {'origins': 'https://your-frontend-domain.com'}}
