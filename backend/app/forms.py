3273a1f69507570e49439239983179b8c7163a2d
dylen400mh <dylenbelanger2004@gmail.com>
2023-11-22 16:25:00 -0500

    Create registration form

--- backend/app/forms.py
+++ backend/app/forms.py
@@ -1,10 +1,13 @@
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
 from flask_wtf import FlaskForm
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
 
-
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     
