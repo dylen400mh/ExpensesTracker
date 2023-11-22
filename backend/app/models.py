20f840abf8296e20af76702ef218add1aa89d5f8
dylen400mh <dylenbelanger2004@gmail.com>
2023-11-21 21:24:22 -0500

    Add constructors to model classes

--- backend/app/models.py
+++ backend/app/models.py
@@ -6,6 +6,10 @@ class User(db.Model):
     username = db.Column(db.String, nullable=False)
     password_hash = db.Column(db.String, nullable=False)
 
+    def __init__(self, username, password):
+        self.username = username
+        self.set_password(password)
+
     def set_password(self, password):
         self.password_hash = pbkdf2_sha256.hash(password)
 
@@ -21,6 +25,12 @@ class Expense(db.Model):
     description = db.Column(db.String)
     category = db.Column(db.String)
     date = db.Column(db.Date)
+
+    def __init__(self, amount, date, description=None, category=None):
+        self.amount = amount
+        self.date = date
+        self.description = description
+        self.category = category
 
     def __repr__(self):
         return f"<Expense(id={self.id}, amount={self.amount}, description={self.description}, category={self.category}, date={self.date})>"
