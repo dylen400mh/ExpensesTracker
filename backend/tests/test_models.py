8a4f6d9847ce694851aa5b40bd83bb8a045874df
dylen400mh <dylenbelanger2004@gmail.com>
2023-11-23 16:19:51 -0500

    Create tests package and test models.py

---
+++ backend/tests/test_models.py
@@ -0,0 +1,46 @@
+from app.models import User, Expense
+from datetime import date
+
+
+def test_create_user():
+    user = User('testuser', 'testpassword')
+
+    assert user.username == 'testuser'
+    assert user.check_password('testpassword')
+
+
+def test_check_password():
+    user = User('testuser', 'testpassword')
+
+    assert user.check_password('testpassword')
+    assert not user.check_password('wrong')
+
+
+def test_set_password():
+    user = User('testuser', 'testpassword')
+
+    user.set_password("newpassword")
+
+    assert user.check_password('newpassword')
+
+
+def test_user_repr():
+    user = User('testuser', 'testpassword')
+
+    assert user.__repr__() == f'<User(id={user.id}, username=testuser)>'
+
+
+def test_create_expense():
+    expense = Expense(100, 'test expense', 'Food', date(2023, 11, 23))
+
+    assert expense.amount == 100
+    assert expense.description == 'test expense'
+    assert expense.category == 'Food'
+    assert expense.date == date(2023, 11, 23)
+
+
+def test_expense_repr():
+    expense = Expense(100, 'test expense', 'Food', date(2023, 11, 23))
+
+    assert expense.__repr__(
+    ) == f"<Expense(id={expense.id}, amount=100, description=test expense, category=Food, date=2023-11-23)>"
